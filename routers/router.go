751743ed08a9240ec5f948df505ea616d23f406f
yealou <yanlsix@163.com>
2024-04-12 23:18:57 +0800

    add swagger

--- routers/router.go
+++ routers/router.go
@@ -1,8 +1,11 @@
 package routers
 
 import (
+	_ "composable-nfts-service/docs"
 	"composable-nfts-service/routers/api"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitRouter() *gin.Engine {
@@ -10,6 +13,8 @@ func InitRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	testApi := r.Group("/test")
 	{
